29f57972242ae84fe80f13fcee2e5ceb761d5bfe
ageon-server1 <ageon144@gmail.com>
2025-04-08 12:10:43 +0530

    Update bot_config.py

--- bot_config.py
+++ bot_config.py
@@ -1,6 +1,6 @@
-BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
-OPENAI_API_KEY = "sk-...YOUR_OPENAI_KEY"
-MONGO_URI = "mongodb://localhost:27017"
+BOT_TOKEN = "7450391744:AAEb__pEdwLYY4zfb7F8WZPDs12XswyEUD8"
+OPENAI_API_KEY = "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"
+MONGO_URI = "mongodb+srv://cluster0.iqsms0c.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&appName=Cluster0"
 
-ADMINS = [123456789]
-RESELLERS = [987654321]
+ADMINS = [6552242136]
+RESELLERS = [6552242136]
